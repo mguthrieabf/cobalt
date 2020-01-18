09413c21f362a2ba7486a77eaa333daa208c0479
Mark Guthrie <mark.guthrie@abf.com.au>
2020-01-18 15:57:00 +1100

    Auto update at 2020-01-18_15:57:00

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -4,6 +4,7 @@ from accounts.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        print("Where does this go")
         if not User.objects.filter(username="admin").exists():
             # User.objects.create_superuser("admin", "admin@admin.com", "admin")
             user = User.objects.create_user(username='admin',
