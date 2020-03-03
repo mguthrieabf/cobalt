439e5f42ed298bddc6ecdce0c719be680d39fb9a
Mark Guthrie <mark.guthrie@abf.com.au>
2020-03-03 14:02:12 +1100

    Auto update at 2020-03-03_14:02:11

--- cobalt/settings.py
+++ cobalt/settings.py
@@ -84,17 +84,17 @@ DATABASES = {
     },
 }
 
-# if 'RDS_HOSTNAME' in os.environ:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': os.environ['RDS_DB_NAME'],
-#             'USER': os.environ['RDS_USERNAME'],
-#             'PASSWORD': os.environ['RDS_PASSWORD'],
-#             'HOST': os.environ['RDS_HOSTNAME'],
-#             'PORT': os.environ['RDS_PORT'],
-#         }
-#     }
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 
     # 'abfmpc_db' : {
     #     'ENGINE': 'sql_server.pyodbc',
