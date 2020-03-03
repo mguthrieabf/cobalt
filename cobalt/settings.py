0b3394908f679fc34a043ee93891974bfc258afa
Mark Guthrie <mark.guthrie@abf.com.au>
2020-03-03 13:28:22 +1100

    Auto update at 2020-03-03_13:28:22

--- cobalt/settings.py
+++ cobalt/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'cobalt.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite34'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
 }
 
