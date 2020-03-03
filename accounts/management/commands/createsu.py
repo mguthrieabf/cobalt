f4277406f37666fd3ea418978e02960603ddc5f6
Mark Guthrie <mark.guthrie@abf.com.au>
2020-03-03 14:26:06 +1100

    Auto update at 2020-03-03_14:26:06

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -4,13 +4,29 @@ from accounts.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print("Where does this go")
+        print("Running createsu")
         if not User.objects.filter(username="admin").exists():
             # User.objects.create_superuser("admin", "admin@admin.com", "admin")
             user = User.objects.create_user(username='admin',
                                  email='a@b.com',
                                  password='F1shcake',
                                  abf_number=99)
+            user.is_superuser=True
+            user.is_staff=True
+            user.save()
             self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
         else:
-            self.stdout.write(self.style.SUCCESS('###########################Super user already exists - ok'))
+            self.stdout.write(self.style.SUCCESS('admin user already exists - ok'))
+
+        if not User.objects.filter(username="MG").exists():
+            # User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            user = User.objects.create_user(username='MG',
+                                 email='m@rkguthrie.com',
+                                 password='F1shcake',
+                                 abf_number=620246)
+            user.is_superuser=True
+            user.is_staff=True
+            user.save()
+            self.stdout.write(self.style.SUCCESS('Successfully created new MG user'))
+        else:
+            self.stdout.write(self.style.SUCCESS('MG user already exists - ok'))
