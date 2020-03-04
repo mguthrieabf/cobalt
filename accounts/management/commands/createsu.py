eb7678ca3f99f8c3fa20d8ffe8a1205a2e4ab716
Mark Guthrie <mark.guthrie@abf.com.au>
2020-03-04 16:43:05 +1100

    Auto update at 2020-03-04_16:43:05

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -3,30 +3,24 @@ from accounts.models import User
 
 class Command(BaseCommand):
 
+    def CreateDefaultTestUsers(self, newuser, email, abf, first, last):
+        if not User.objects.filter(username=newuser).exists():
+            user = User.objects.create_user(username=newuser,
+                                 email=email,
+                                 password='F1shcake',
+                                 first_name=first,
+                                 last_name=last,
+                                 abf_number=abf)
+            user.is_superuser=True
+            user.is_staff=True
+            user.save()
+            self.stdout.write(self.style.SUCCESS('Successfully created new super user - %s' % newuser))
+        else:
+            self.stdout.write(self.style.SUCCESS('%s user already exists - ok' % newuser))
+
+
     def handle(self, *args, **options):
         print("Running createsu")
-        if not User.objects.filter(username="admin").exists():
-            # User.objects.create_superuser("admin", "admin@admin.com", "admin")
-            user = User.objects.create_user(username='admin',
-                                 email='a@b.com',
-                                 password='F1shcake',
-                                 abf_number=99)
-            user.is_superuser=True
-            user.is_staff=True
-            user.save()
-            self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
-        else:
-            self.stdout.write(self.style.SUCCESS('admin user already exists - ok'))
-
-        if not User.objects.filter(username="MG").exists():
-            # User.objects.create_superuser("admin", "admin@admin.com", "admin")
-            user = User.objects.create_user(username='MG',
-                                 email='m@rkguthrie.com',
-                                 password='F1shcake',
-                                 abf_number=620246)
-            user.is_superuser=True
-            user.is_staff=True
-            user.save()
-            self.stdout.write(self.style.SUCCESS('Successfully created new MG user'))
-        else:
-            self.stdout.write(self.style.SUCCESS('MG user already exists - ok'))
+        self.CreateDefaultTestUsers("admin", "a@b.com", "99", "Admin", "Admin")
+        self.CreateDefaultTestUsers("Mark", "m@rkguthrie.com", "620246", "Mark", "Guthrie")
+        self.CreateDefaultTestUsers("Julian", "julianrfoster@gmail.com", "518891", "Julian", "Foster")
