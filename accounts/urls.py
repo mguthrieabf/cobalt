27d68c698c3204ac8ef4c3d1240f06b8d0f5fae1
Mark Guthrie <mark.guthrie@abf.com.au>
2020-03-25 09:41:17 +1100

    improved accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,6 @@
 from django.conf.urls import url
 from . import views
 
-# urlpatterns = [
-#     url('signup/', views.signup, name='signup'),
-#     url('signup2/', views.signup2, name='signup2'),
-#     url('login', views.login, name='login'),
-#     url('logout', views.logout, name='logout'),
-# ]
-
 urlpatterns = [
     url('register', views.register, name='register'),
     url('loggedout', views.loggedout, name='loggedout'),
