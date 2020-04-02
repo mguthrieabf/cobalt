1333c8c01d53b288753086e1cf0a608abc97f847
Mark Guthrie <mark.guthrie@abf.com.au>
2020-04-03 07:38:46 +1100

    Auto update at 2020-04-03_07:38:46

--- forums/static/forums/summernote/ResizeSensor.js
+++ forums/static/forums/summernote/ResizeSensor.js
@@ -1,4 +1,4 @@
-/**
+/** A
  * Copyright Marc J. Schmidt. See the LICENSE file at the top-level
  * directory of this distribution and at
  * https://github.com/marcj/css-element-queries/blob/master/LICENSE.
