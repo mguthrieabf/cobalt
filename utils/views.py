f4b5d5eb9ffa219e548ea9b597c3c2dd04c6f9a9
Mark Guthrie <mark.guthrie@abf.com.au>
2020-08-04 14:38:03 +1000

    Auto update from XPS13

--- utils/views.py
+++ utils/views.py
@@ -1,34 +1,15 @@
 # from django.shortcuts import render
 from geopy.geocoders import Nominatim
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import json
 
 @login_required()
 def geo_location(request, location):
     """ return lat and long for a text address """
 
-    print(location)
-
-    # if request.method == "POST":
-    #
-    #     print(request.body)
-    #     # print(request.POST.get('location'))
-    #     # print(request.body['location'])
-    #     data = json.loads(request.body)
-    #     print(data)
-    #    # response_json = request.POST
-       # response_json = json.dumps(response_json)
-       # data = json.loads(response_json)
-       # print(data)
-       #  # data = request.body.decode('utf-8')
-        # print("=%s=" % data)
-        # data = json.loads(request.POST.get('data', ''))
-        # print("=%s=" % data)
-        # if "address" in request.POST:
-        #     address = request.POST["address"]
-        #     geolocator = Nominatim(user_agent="cobalt")
-        #     location = geolocator.geocode(address)
-        #     print(location.latitude, location.longitude)
-
-    return HttpResponse({"status": "hello"})
+    geolocator = Nominatim(user_agent="cobalt")
+    loc = geolocator.geocode(location)
+    html={"lat": loc.latitude, "lon": loc.longitude}
+    data_dict = {"data": html}
+    return JsonResponse(data=data_dict, safe=False)
