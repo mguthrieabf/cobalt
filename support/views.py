9ee19019b04c5a8c48c4acaf216f4f920bcb1205
Mark Guthrie <mark.guthrie@abf.com.au>
2020-08-23 14:33:40 +1000

    test data

--- support/views.py
+++ support/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from notifications.views import send_cobalt_email
+from cobalt.settings import COBALT_HOSTNAME
 
 # from django.http import HttpResponse
 # from django.utils import timezone
@@ -15,4 +17,5 @@ def rebuild(request):
     f = open("/tmp/trigger.txt", "w")
     f.write("ok")
     f.close()
+    send_cobalt_email('m@rkguthrie.com', 'Test Data Reset on %s by %s' % (COBALT_HOSTNAME, request.user.first_name), " ")
     return render(request, "support/rebuild.html")
