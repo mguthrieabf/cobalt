f1a60fd27b0e95c4cdc127d0a9a7fa902f6dfb90
Mark Guthrie <mark.guthrie@abf.com.au>
2020-08-28 14:34:29 +1000

    eb config

--- .platform/hooks/postdeploy/02_django.sh
+++ .platform/hooks/postdeploy/02_django.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-cat /opt/elasticbeanstalk/deployment/env >> /tmp/migrate 2>&1
+# commands to run after each deployment
+# These MUST be re-runnable (not fail or create duplicates if run again)
 
 . /var/app/venv/staging-LQM1lest/bin/activate
-python manage.py migrate >> /tmp/migrate 2>&1 
+
+./manage.py migrate
+./manage.py collectstatic --noinput
+./manage.py createsu
+./manage.py create_abf
+./manage.py add_rbac_static_forums
+./manage.py add_rbac_static_payments
+./manage.py add_rbac_static_orgs
+./manage.py add_rbac_static_events
+./manage.py add_test_data
+./manage.py create_states
+#./manage.py createdummyusers
+#./manage.py importclubs
