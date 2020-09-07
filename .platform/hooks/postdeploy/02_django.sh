fbd996ecf1c84e75f984f74d03baa3495dc19a4e
Mark Guthrie <mark.guthrie@abf.com.au>
2020-09-07 21:47:34 +1000

    test data

--- .platform/hooks/postdeploy/02_django.sh
+++ .platform/hooks/postdeploy/02_django.sh
@@ -13,7 +13,7 @@
 ./manage.py add_rbac_static_payments
 ./manage.py add_rbac_static_orgs
 ./manage.py add_rbac_static_events
-./manage.py add_test_data
 ./manage.py create_states
+#./manage.py add_test_data
 #./manage.py createdummyusers
 #./manage.py importclubs
