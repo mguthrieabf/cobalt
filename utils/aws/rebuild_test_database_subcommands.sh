524d4bd46cb27d9871026c7f5cee95cbb9b3527e
Mark Guthrie <m@rkguthrie.com>
2020-11-05 13:52:22 +1100

    test data

--- utils/aws/rebuild_test_database_subcommands.sh
+++ utils/aws/rebuild_test_database_subcommands.sh
@@ -7,7 +7,8 @@
 ./manage.py add_rbac_static_payments
 ./manage.py add_rbac_static_orgs
 ./manage.py add_rbac_static_events
+./manage.py add_rbac_static_notifications
 ./manage.py create_states
 ./manage.py add_test_data
 #./manage.py createdummyusers
-./manage.py importclubs
+#./manage.py importclubs
