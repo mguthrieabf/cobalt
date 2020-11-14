fb1e8d6090d3ff759604f91fa5e8b8ec068b62ee
Mark Guthrie <mark.guthrie@abf.com.au>
2020-11-14 11:33:56 +1100

    logged out

--- cobalt/context_processors.py
+++ cobalt/context_processors.py
@@ -28,6 +28,7 @@ def global_settings(request):
         "GLOBAL_COOKIES": settings.GLOBAL_COOKIES,
         "GLOBAL_MPSERVER": settings.GLOBAL_MPSERVER,
         "GLOBAL_PRODUCTION": settings.GLOBAL_PRODUCTION,
+        "GLOBAL_PRIVACY": settings.GLOBAL_PRIVACY,
         "GLOBAL_CURRENCY_SYMBOL": settings.GLOBAL_CURRENCY_SYMBOL,
         "GLOBAL_CURRENCY_NAME": settings.GLOBAL_CURRENCY_NAME,
         "AUTO_TOP_UP_MAX_AMT": settings.AUTO_TOP_UP_MAX_AMT,
