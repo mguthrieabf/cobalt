c70a2568d38296cce9165fc343b7e9753550be43
Mark Guthrie <mark.guthrie@abf.com.au>
2020-12-28 11:42:41 +1100

    certs4

--- cobalt/settings.py
+++ cobalt/settings.py
@@ -127,7 +127,7 @@ DUMMY_DATA_COUNT = int(set_value("DUMMY_DATA_COUNT", 20))
 #########################################
 ALLOWED_HOSTS = [
     ".abftech.com.au",
-    ".myabf.com.au",
+    "test.myabf.com.au",
     "127.0.0.1",
     "bs-local.com",
     "localhost",
